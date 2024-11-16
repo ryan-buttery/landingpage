9ccd6a66272b0080e3468e4481ce3f8a9a226df3
Ryan Buttery <21200135+ryan-buttery@users.noreply.github.com>
2024-11-16 16:16:36 +0100

    PWA and it actually works wtf

---
+++ public-html/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'ryanbuttery-cache-v1';
+const urlsToCache = [
+  './',
+  './styles/bootstrap.min.css',
+  './styles/style.css',
+  './images/logo.webp',
+  // Add paths to other critical resources here.
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return the response from the cache
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
